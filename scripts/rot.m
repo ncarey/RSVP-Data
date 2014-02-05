f2b0f45f04f7ea1a6798a2fce1fda154777ca023
ncarey <ncarey90@gmail.com>
2014-02-05 14:35:16 -0500

    added display of each permutation of dimensions of rotated data

--- scripts/rot.m
+++ scripts/rot.m
@@ -1,42 +1,44 @@
-#read in filename to save image to
-#arglist = argv ();
-#filename = arglist{1};
+#read in filename to save image to; read in number of dimensions in data
+arglist = argv ();
+filename = arglist{1};
+dims = str2num(arglist{2});
 
-x = dlmread('datapoints/x.txt',',')
-y = dlmread('datapoints/y.txt',',')
-z = dlmread('datapoints/z.txt',',')
+data = []
 
-#x = [1,1,-1,-1,1,1,-1,-1]
-#y = [1,-1,-1,1,1,-1,-1,1]
-#z = [1,1,1,1,-1,-1,-1,-1]
-
-
-
-#generate random rotation matrix
-
-[Q,R] = qr(randn(3))
-Q = Q*diag(sign(diag(R)))
-
-rotx=[]
-roty=[]
-rotz=[]
-
-for i=1:(length(x))
-  point=[x(:,i);y(:,i);z(:,i)]
-  rot_p = Q*point
-  rotx = [rotx,rot_p(1,:)]
-  roty = [roty,rot_p(2,:)]
-  rotz = [rotz,rot_p(3,:)]
+for i=1:dims
+  data = [data;dlmread(sprintf('datapoints/dim%i.txt',i),',')]
 end
 
 
+#generate random rotation matrix
+# code taken from http://www.mathworks.com/matlabcentral/newsreader/view_thread/298500
 
-#add in integer after xyz to control size of datapoints. Default seems fine for now
-#scatter3(x,y,z,5)
+[Q,R] = qr(randn(dims))
+Q = Q*diag(sign(diag(R)))
+
+rotdata = []
+
+#iterate thru points
+for i=1:(length(data(1,:)))
+  point = data(:,i)
+  rotdata = [rotdata,Q*point]
+end
+
+#iterate thru dimensions and print each pair of dims
+for i=1:(length(data(:,1)))
+  for j=(i+1):(length(data(:,1)))
+    scatter(rotdata(i,:), rotdata(j,:))
+    axis("off")
+    print (sprintf('%s.%i-%i.png',filename,i,j), "-dpng")
+  end
+end
+
 
 #scatter3(x,y,z)
-#scatter3(rotx,roty,rotz)
-scatter(roty,rotz)
+#scatter3(rotdata(1,:),rotdata(2,:),rotdata(3,:))
+#scatter3(data(1,:),data(2,:),data(3,:))
+#scatter(roty,rotz)
+
 #axis("off")
 
 #print -dpng derp
